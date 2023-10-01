6d4cfc1df01b3bafa7067f10ef51fea7d6fc43c5
brianbryank <brianbryan0125@gmail.com>
2023-10-01 13:16:53 +0300

    passed

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,40 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
+
+
+
+    @classmethod
+    def create_table(self):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs ( 
+            id INTERGER PRIMARY KEY,
+            name TEXT,
+            album TEXT
+            )
+        
+"""
+        CURSOR.execute(sql)
+
+
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.album))
+
+        CONN.commit()
+    
